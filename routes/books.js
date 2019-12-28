113f03db031007feaa54f54708d0da63325ae72a
blablamedia <pierre@gambaud.com>
2019-12-28 16:31:58 +0100

    add books controller

--- routes/books.js
+++ routes/books.js
@@ -1,87 +1,27 @@
 const express = require(`express`);
 const router = express.Router();
 
-const Book = require(`../models/book.js`);
+const booksController = require('../controllers/books.js');
 
 
 // index
-router.get(`/`, (req, res, next) => {
-  Book.find()
-    .then(books => {
-      res.render(`books/index`, { books })
-    })
-    .catch(next);
-});
+router.get(`/`, booksController.index);
 
 
 // create
-router.post(`/create`, (req, res, next) => {
-  const { isbn, isbn13, asin, title, authors, coverPath, publisher, publicationDate } = req.body;
-
-  Book.create({
-    isbn,
-    isbn13,
-    asin,
-    title,
-    authors,
-    coverPath,
-    publisher,
-    publicationDate
-  })
-    .then(book => res.redirect(`/`))
-    .catch(next);
-});
+router.post(`/create`, booksController.create);
 
 
 // show
-router.get(`/:id`, (req, res, next) => {
-  if(!req.params.id) {
-    return next(new Error('This ID does not match any book'));
-  }
-
-  const id = req.params.id;
-
-  Book.findById(id)
-    .then(book => {
-      res.render(`books/show`, book);
-    })
-    .catch(next);
-});
+router.get(`/:id`, booksController.show);
 
 
 // update
-router.put(`/:id`, (req, res, next) => {
-  if(!req.params.id) {
-    return next(new Error('This ID does not match any book'));
-  }
-
-  const id = req.params.id;
-  const { isbn, isbn13, asin, title, authors, coverPath, publisher, publicationDate } = req.body;
-  
-  Book.findByIdAndUpdate(id, {
-    isbn,
-    isbn13,
-    asin,
-    title,
-    authors,
-    coverPath,
-    publisher,
-    publicationDate
-  })
-    .then(book => res.redirect(`/`))
-    .catch(next);
-});
+router.put(`/:id`, booksController.update);
 
 
 // destroy
-router.delete(`/:id`, (req, res, next) => {
-  if(!req.params.id) {
-    return next(new Error('This ID does not match any book'));
-  }
+router.delete(`/:id`, booksController.destroy);
 
-  const id = req.params.id;
-  
-  Book.findByIdAndDelete(id)
-    .then(book => res.redirect(`/`))
-    .catch(next);
-});
+
+module.exports = router;
