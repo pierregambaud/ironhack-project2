e6c292e337f6550fb6d1999fe595157b9873695a
blablamedia <pierre@gambaud.com>
2019-12-30 19:32:29 +0100

    update API routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,8 +5,8 @@ const booksRoutes = require(`./books`);
 const reviewsRoutes = require(`./reviews`);
 const usersRoutes = require(`./users`);
 
-router.use('/books/', booksRoutes);
-router.use('/reviews/', reviewsRoutes);
-router.use('/users/', usersRoutes);
+router.use('/books', booksRoutes);
+router.use('/reviews', reviewsRoutes);
+router.use('/users', usersRoutes);
 
 module.exports = router;
