21a77ab399497c32016e8d46eb676da5b536a86e
blablamedia <pierre@gambaud.com>
2020-01-02 22:27:21 +0100

    add index page and style

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+//    _           _           
+//   (_)         | |          
+//    _ _ __   __| | _____  __
+//   | | '_ \ / _` |/ _ \ \/ /
+//   | | | | | (_| |  __/>  < 
+//   |_|_| |_|\__,_|\___/_/\_\
+//   
 
-  console.log('IronGenerator JS imported successfully!');
+const emailInput = document.querySelector(`input[name="email"]`);
+const passwordInput = document.querySelector(`input[name="password"]`);
+const submitButton = document.querySelector(`button`);
+const socialLogins = document.getElementById(`socialLogins`);
 
-}, false);
+emailInput.addEventListener('focus', () => {
+  passwordInput.classList.remove(`isHidden`);
+  socialLogins.style.paddingTop = "3em";
+});
+
+passwordInput.addEventListener('focus', () => {
+  submitButton.classList.remove(`isHidden`); 
+});
