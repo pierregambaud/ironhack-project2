75a79e038004672f01250ad2fab455d70bb2b816
blablamedia <pierre@gambaud.com>
2020-01-03 20:17:22 +0100

    add member index and sticky nav

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,21 @@ const router  = express.Router();
 
 // visitor homepage
 router.get(`/`, (req, res, next) => {
-  res.render(`index`);
+  res.render(`index/visitor`);
 });
+
 
 // signup homepage
 router.get(`/inscription`, (req, res, next) => {
   res.render(`signup`);
+});
+
+
+// member homepage => FIXME: need to merge with visitor when auth with API created
+router.get(`/m`, (req, res, next) => {
+  res.render(`index/member`, {
+    layout: 'member-layout'
+  });
 });
 
 
