b4c5ac16ff7399999ca1044638982bfef413bb79
blablamedia <pierre@gambaud.com>
2020-01-03 13:22:14 +0100

    redesign index

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,13 +9,15 @@
 const emailInput = document.querySelector(`input[name="email"]`);
 const passwordInput = document.querySelector(`input[name="password"]`);
 const submitButton = document.querySelector(`button`);
-const socialLogins = document.getElementById(`socialLogins`);
+const socialLogins = document.getElementById(`social-logins`);
 
 emailInput.addEventListener('focus', () => {
-  passwordInput.classList.remove(`isHidden`);
+  passwordInput.classList.remove(`is-hidden`);
   socialLogins.style.paddingTop = "3em";
+  socialLogins.classList.add(`slide-bottom`);
+  submitButton.classList.remove(`is-hidden`);
 });
 
-passwordInput.addEventListener('focus', () => {
-  submitButton.classList.remove(`isHidden`); 
+passwordInput.addEventListener('focus', () => { // to fix email input shadow that overlays password input when writing
+  emailInput.style.zIndex = 2;
 });
