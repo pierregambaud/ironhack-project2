19eb698f05e8f61fe991050128b4639b0da47822
blablamedia <pierre@gambaud.com>
2020-01-04 19:38:30 +0100

    delete animation on login

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,19 +5,3 @@
 //   | | | | | (_| |  __/>  < 
 //   |_|_| |_|\__,_|\___/_/\_\
 //   
-
-const emailInput = document.querySelector(`input[name="email"]`);
-const passwordInput = document.querySelector(`input[name="password"]`);
-const submitButton = document.querySelector(`button`);
-const dynamicBloc = document.getElementById(`dynamic-bloc`);
-
-emailInput.addEventListener('focus', () => {
-  passwordInput.classList.remove(`is-hidden`);
-  dynamicBloc.style.paddingTop = "3em";
-  dynamicBloc.classList.add(`slide-bottom`);
-  submitButton.classList.remove(`is-hidden`);
-});
-
-passwordInput.addEventListener('focus', () => { // to fix email input shadow that overlays password input when writing
-  emailInput.style.zIndex = 2;
-});
